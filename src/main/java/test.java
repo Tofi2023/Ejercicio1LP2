ff56a6f653885ff7bb72459a963a894ab27684d3
ronny <ronymedina974@gmail.com>
2024-06-16 11:13:40 -0500

    Initial commit

---
+++ src/main/java/test.java
@@ -0,0 +1,61 @@
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+
+import java.sql.ResultSet;
+
+import java.sql.SQLException;
+
+import java.sql.Statement;
+
+
+
+public class test {
+
+
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+		System.out.println("Proyecto con Maven \n ------------------------");
+
+		
+
+		try {
+
+			Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/instituto?useSSL=false","root", "estofaneromedina15");
+
+			if(con!=null) {
+
+				System.out.println("SE CONECTO CON LA BASE DE DATOS");
+
+								
+
+				Statement sentencia=con.createStatement();
+
+	            ResultSet resultado=sentencia.executeQuery("SELECT * FROM user");
+
+	            while (resultado.next())
+
+	            {
+
+	                System.out.println (resultado.getString (1) + " " + resultado.getString(2));
+
+	            }
+
+			}
+			
+		} catch (Exception e) {
+
+			// TODO Auto-generated catch block
+
+			e.printStackTrace();
+
+			System.out.println("ERROR! NO SE CONECTO CON LA BASE DE DATOS");
+
+		}
+
+	}
+
+}
